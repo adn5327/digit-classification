ece00ef57c0d83688990edd5f1e036ec591b5fb3
Jakub Klapacz <kubaklapacz@gmail.com>
2015-12-06 20:57:59 -0600

    Added extra features

--- perceptrons/main.py
+++ perceptrons/main.py
@@ -1,7 +1,9 @@
 import numpy as np
 from svm import *
+import multiclass as mc
 import time
 import pickle 
+from sys import argv
 
 def process_image(data_filename = "../digitdata/trainingimages", label_filename = "../digitdata/traininglabels"):
 	labels = open(label_filename, "r")
@@ -21,8 +23,8 @@ def process_image(data_filename = "../digitdata/trainingimages", label_filename 
 			for j in range(28):
 				if cur_line[j] == ' ':
 					vectorized_image.append(0)
-				elif cur_line[j] == '+':
-					vectorized_image.append(2)
+				# elif cur_line[j] == '+':
+					# vectorized_image.append(2)
 				else:
 					vectorized_image.append(1)
 		x.append(vectorized_image)	
@@ -41,7 +43,7 @@ def process_image(data_filename = "../digitdata/trainingimages", label_filename 
 	images.close()
 	return ret_y, ret_x
 
-def main(load = 1):
+def main(load = 1, multiclass = 0):
 
 	# Here y is a vector of labels, X is a vector of images. The same indicies of y and X correspond to one image.
 	# y, X = process_image("../digitdata/smallimages", "../digitdata/smalllabels")
@@ -56,39 +58,96 @@ def main(load = 1):
 	# 	print testy[i]
 	# 	print 
 	# 	print classifier_0.predict(testX[i])
-	if load:
-		classifiers = pickle.load(open("perceptrons.data", "rb"))
+	if multiclass == 0:
+		if load:
+			classifiers = pickle.load(open("perceptrons.data", "rb"))
+		else:
+			classifiers = list()
+			for category in np.unique(y):
+				start = time.clock()
+				classifiers.append(SVC(category, .001))
+				print classifiers[category].category_
+				classifiers[category].fit(X, y)
+				print "Time Elapsed:\n"
+				print time.clock() - start
+			pickle.dump(classifiers, open("perceptrons.data", "wb"))
+			# print classifiers[category].classes_
+		
+		# testy, testX = process_image("../digitdata/smalltestimages", "../digitdata/smalltestlabels")
+		testy, testX = process_image("../digitdata/testimages", "../digitdata/testlabels")
+		# for i in range(len(testX)):
+		total = 0
+		correct = 0
+		for i in range(len(testy)):
+			# print testy[i]
+			results = list()
+			for j in range(len(classifiers)):
+				# print j
+				# print classifiers[i].bias_
+				# print classifiers[i].category_
+				results.append(classifiers[j].predict(testX[i]))
+			# print results
+			total += 1
+			if results.index(max(results)) == testy[i]:
+				correct += 1
+
+			# print results.index(max(results))
+			# print classifiers[0].predict(testX[i])
+			# print classifiers[9].predict(testX[i])
+		accuracy = (correct * 1.0) / (total * 1.0)
+		print 'accuracy = {0:.4%}'.format(accuracy)
 	else:
-		classifiers = list()
-		for category in np.unique(y):
-			start = time.clock()
-			classifiers.append(SVC(category))
-			print classifiers[category].category_
-			classifiers[category].fit(X, y)
-			print "Time Elapsed:\n"
-			print time.clock() - start
-		pickle.dump(classifiers, open("perceptrons.data", "wb"))
-		# print classifiers[category].classes_
-	
-	# testy, testX = process_image("../digitdata/smalltestimages", "../digitdata/smalltestlabels")
-	testy, testX = process_image("../digitdata/testimages", "../digitdata/testlabels")
-	# for i in range(len(testX)):
-	for i in range(10):
-		# print testy[i]
-		results = list()
-		for j in range(10):
-			# print j
-			# print classifiers[i].bias_
-			# print classifiers[i].category_
+		if load:
+			classifiers = pickle.load(open("mc.perceptrons.data", "rb"))
+		else:
+			classifier = mc.SVC(.001)
+			classifier.fit(X, y)
+			
+
+			# for category in np.unique(y):
+			# 	start = time.clock()
+			# 	classifiers.append(SVC(category, .001))
+			# 	print classifiers[category].category_
+			# 	classifiers[category].fit(X, y)
+			# 	print "Time Elapsed:\n"
+			# 	print time.clock() - start
+			pickle.dump(classifiers, open("mc.perceptrons.data", "wb"))
+			# print classifiers[category].classes_
+		
+		# testy, testX = process_image("../digitdata/smalltestimages", "../digitdata/smalltestlabels")
+		testy, testX = process_image("../digitdata/testimages", "../digitdata/testlabels")
+		# for i in range(len(testX)):
+		total = 0
+		correct = 0
+		for i in range(len(testy)):
+			# print testy[i]
+			
+			
+				# print j
+				# print classifiers[i].bias_
+				# print classifiers[i].category_
 			results.append(classifiers[j].predict(testX[i]))
-		print results
-		print results.index(max(results))
-		# print classifiers[0].predict(testX[i])
-		# print classifiers[9].predict(testX[i])
+			# print results
+			result = classifier.predict(testX[i])
+			total += 1
+			if result == testy[i]:
+				correct += 1
 
-
+			# print results.index(max(results))
+			# print classifiers[0].predict(testX[i])
+			# print classifiers[9].predict(testX[i])
+		accuracy = (correct * 1.0) / (total * 1.0)
+		print 'accuracy = {0:.4%}'.format(accuracy)
 
 if __name__ == '__main__':
 	start = time.clock()
-	main()
+	if len(argv) == 3 and argv[1] == '-r' and argv[2] == '-m':
+		main(0, 1)
+	elif len(argv) == 2 and argv[1] == '-r':
+		main(0, 0)
+	elif len(argv) == 2 and argv[1] == '-m':
+		main(1, 1)
+	else:
+		main(1, 0)
+	# main()
 	print time.clock() - start
--- perceptrons/multiclass.py
+++ perceptrons/multiclass.py
@@ -28,20 +28,20 @@ class SVC(object):
         The number of classes in the dataset.
     """
 
-    def __init__(self, category, reg=1):
+    def __init__(self, reg=1):
         self.reg = reg
 
         # It's just my habit to suffix variables with '_' if they're
         # instantiated in the "fitting" stage of the algorithm.
         # This is how the scikit-learn maintainers style their code.
-        self.weights_ = None
-        self.bias_ = 0
+        self.weights_ = list()
+        self.bias_ = np.zeros((1,10))
         self.n_features_ = None
         self.classes_ = None
-        self.category_ = category
+        # self.category_ = category
 
-    def f(self, x):
-            result = ((self.weights_.dot(x)) + self.bias_)
+    def f(self, x, i):
+            result = ((self.weights_[i].dot(x)) + self.bias_[i])
             # quit()
             return result
 
@@ -57,29 +57,32 @@ class SVC(object):
         # assert len(self.classes_) == 2
 
         # Converts the binary labels of y into -1 and 1.
-        new_y = np.zeros(len(y))
-        new_y[y != self.category_] = -1
-        new_y[y == self.category_] = 1
+        new_y = np.zeros((10, len(y)))
+        for i in range(10):
+            new_y[i][y != i] = -1
+            new_y[i][y == i] = 1
+        # new_y[y != self.category_] = -1
+        # new_y[y == self.category_] = 1
         y = new_y
 
         # Shuffle the training set
         perm = np.random.permutation(len(X))
         X = X[perm]
-        y = y[perm]
+        y = y[:][perm]
 
         # Create the validation set
         val_ind = len(X) * validation_size
         val_X = X[:val_ind]
-        val_y = y[:val_ind]
+        val_y = y[:][:val_ind]
         X = X[val_ind:]
-        y = y[val_ind:]
+        y = y[:][val_ind:]
         self.n_features_ = X.shape[1]
         # Initialize weights to between [-1, 1)
-        # self.weights_ = np.random.random(X.shape[1]) * 2 - 1
-        self.weights_ = np.zeros(X.shape[1])
+        for i in range(10):
+            self.weights_.append(np.random.random(X.shape[1]) * 2 - 1)
+            self.bias_[i] = random.random()*2-1
         # print len(self.weights_)
-        self.bias_ = random.random()*2-1
-        # self.bias_ = 0
+        # self.bias_ = random.random()*2-1
         alpha = 1
         beta = 1
 
@@ -89,39 +92,46 @@ class SVC(object):
             for i in range(10):
                 for j in range(10):
                     rand_idx = np.random.choice(np.arange(len(X)))
-                    cur_X, cur_y = X[rand_idx], y[rand_idx]
-                    if((cur_y * self.f(cur_X)) >= 1):
-                        grad_w = self.reg * self.weights_
-                        grad_b = 0
-                    else:
-                        grad_w = (self.reg * self.weights_) - (cur_y * cur_X)
-                        grad_b = -cur_y
-                    self.weights_ = self.weights_ - (step * grad_w)
-                    self.bias_ = self.bias_ - (step * grad_b)
-                total = 0
-                correct = 0
-                for val_i in range(len(val_X)):
-                    total += 1
-                    cur_X = val_X[val_i]
-                    cur_y = val_y[val_i]
-                    # print cur_X
-                    if (self.predict(cur_X) == cur_y):
-                        correct += 1
-                accuracy = (correct * 1.0) / (total * 1.0)
-                if(i % 10 == 0):
-                    validation_accuracy.append([self.reg, cur_epoch, accuracy])
-        matrix = np.matrix(validation_accuracy)
-        x = matrix[:, 1]
-        x = x.T
-        x = np.asarray(x)
-        # print x[0]
-        y = matrix[:, 2]
-        y = y.T
-        y = np.asarray(y)
-        # print y[0]
-        # print x.shape
+                    cur_X = X[rand_idx]
+                    results = list()
+                    for k in range(10):
+                        cur_y = y[k][rand_idx]
+                        # results.append(self.f(cur_X, k))
+                        
+                    # predicted_result = results.index(max(results))    
+
+                        if((cur_y * self.f(cur_X, k)) >= 1):
+                            grad_w = self.reg * self.weights_[k]
+                            grad_b = 0
+                        else:
+                            grad_w = (self.reg * self.weights_[k]) - (cur_y * cur_X)
+                            grad_b = -cur_y
+                        self.weights_[k] = self.weights_[k] - (step * grad_w)
+                        self.bias_[k] = self.bias_[k] - (step * grad_b)
+                # total = 0
+                # correct = 0
+                # for val_i in range(len(val_X)):
+                #     total += 1
+                #     cur_X = val_X[val_i]
+                #     cur_y = val_y[val_i]
+                #     # print cur_X
+                #     if (self.predict(cur_X) == cur_y):
+                #         correct += 1
+                # accuracy = (correct * 1.0) / (total * 1.0)
+                # if(i % 10 == 0):
+                #     validation_accuracy.append([self.reg, cur_epoch, accuracy])
+        # matrix = np.matrix(validation_accuracy)
+        # x = matrix[:, 1]
+        # x = x.T
+        # x = np.asarray(x)
+        # # print x[0]
+        # y = matrix[:, 2]
+        # y = y.T
+        # y = np.asarray(y)
+        # # print y[0]
+        # # print x.shape
         
-        return (x, y)
+        # return (x, y)
 
         # data = [
         #     go.Scatter(
@@ -154,11 +164,15 @@ class SVC(object):
         # print self.n_features_
 
         assert X.shape[1] == self.n_features_
-        result = self.weights_.dot(X.T) + self.bias_
+        results = list()
+        for i in range(10):
+            results.append(self.weights_[k].dot(X.T) + self.bias_[k])
+        # result = self.weights_.dot(X.T) + self.bias_
+        return results.index(max(results))
         # print result
-        return result
-        assert result != 0
-        if result > 0:
-            return 1
-        else:
-            return -1
+        # return result
+        # assert result != 0
+        # if result > 0:
+            # return 1
+        # else:
+            # return -1
